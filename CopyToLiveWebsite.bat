8c53c1c950f0bbde584bb2e79b92dd03b687bb8a
mgledhill <mg@practicalseries.com>
2025-05-26 14:34:49 +0100

    🚧 Repo construction — Master (uncompressed) versions made

---
+++ CopyToLiveWebsite.bat
@@ -0,0 +1,3 @@
+rd "\\192.168.1.85\01 Pa_Clavis\2231 MasteringGitHub Website\9 Website Live" /s /q
+md "\\192.168.1.85\01 Pa_Clavis\2231 MasteringGitHub Website\9 Website Live"
+robocopy "D:\Dropbox\2500 Git Projects\PS2231 Mastering-GitHHub-Repo\website" "\\192.168.1.85\01 Pa_Clavis\2231 MasteringGitHub Website\9 Website Live" /mir /xf #m-*.*
